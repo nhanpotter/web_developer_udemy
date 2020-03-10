ba0be79268dfc1d6e27c628479088856ab770050
Nhan <nhanpotter8a@gmail.com>
2020-03-10 23:28:41 +0800

    Add Dumb Line

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,3 +3,5 @@ var lastName = prompt("First name?");
 var age = prompt("Age?");
 console.log("Your full name is "+firstName+" "+lastName);
 console.log("Age "+age);
+
+n;oaijsr;ejkawe rjqaew t;jqew tj kj
